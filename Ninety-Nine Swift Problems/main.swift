ed5ca3f685e17f76b96cef7d15431f99ccef7df2
Thomas Augenstein <t.augenstein@mac.com>
2018-03-23 15:19:28 +0100

    Initial Commit

---
+++ Ninety-Nine Swift Problems/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Ninety-Nine Swift Problems
+//
+//  Created by Thomas Augenstein on 23.03.18.
+//  Copyright © 2018 Thomas Augenstein. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
